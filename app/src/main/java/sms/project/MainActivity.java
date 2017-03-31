96d270e753647b578d3d064348e3f3b9a48c7277
Lauri Koskinen <koskinen.late@gmail.com>
2017-03-31 12:03:41 +0300

    final

--- app/src/main/java/sms/project/MainActivity.java
+++ app/src/main/java/sms/project/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -14,8 +13,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
     Button buttonSend;
-    EditText textPhoneNo;
-    EditText textSMS;
+    EditText textPhoneNo, textSMS;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,23 +25,20 @@ public class MainActivity extends Activity {
         textSMS = (EditText) findViewById(R.id.editTextSMS);
 
         buttonSend.setOnClickListener(new OnClickListener() {
-
             @Override
             public void onClick(View v) {
+                String phoneNumber = textPhoneNo.getText().toString();
+                String message = textSMS.getText().toString();
 
-                try {
-
-                    String phoneNumber = textPhoneNo.getText().toString();
-                    String message = textSMS.getText().toString();
+                if (!phoneNumber.isEmpty() && !message.isEmpty() ) {
                     Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumber));
                     intent.putExtra("sms_body", message);
                     startActivity(intent);
 
-                } catch (Exception e) {
+                } else {
                     Toast.makeText(getApplicationContext(),
                             "SMS failed, please try again later!",
                             Toast.LENGTH_LONG).show();
-                    e.printStackTrace();
                 }
             }
         });
